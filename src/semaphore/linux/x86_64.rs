e422d3397d57ef2dfcef887fb6adf5edefbae43c
Julian Orth <ju.orth@gmail.com>
2014-07-22 17:35:42 +0200

    semaphore.h

--- src/semaphore/linux/x86_64.rs
+++ src/semaphore/linux/x86_64.rs
@@ -3,4 +3,6 @@ pub struct sem_t {
     _data: [u64, ..4u],
 }
 
+new!(sem_t)
+
 pub static SEM_FAILED: *mut sem_t = 0 as *mut _;
--- src/semaphore/mod.rs
+++ src/semaphore/mod.rs
@@ -1,58 +1,61 @@
 pub use self::os::{sem_t};
 pub use self::os::{SEM_FAILED};
 
+use {NTStr, int_t, uint_t, char_t};
+use sys::types::{mode_t};
+use time::{timespec};
+
 #[cfg(target_os = "linux")]
 #[path = "linux/mod.rs"]
 mod os;
 
-pub fn sem_close(sem: *mut sem_t) -> ::int_t {
-    extern { fn sem_close(sem: *mut sem_t) -> ::int_t; }
+pub fn sem_close(sem: *mut sem_t) -> int_t {
+    extern { fn sem_close(sem: *mut sem_t) -> int_t; }
     unsafe { sem_close(sem) }
 }
 
-pub fn sem_destroy(sem: *mut sem_t) -> ::int_t {
-    extern { fn sem_destroy(sem: *mut sem_t) -> ::int_t; }
+pub fn sem_destroy(sem: *mut sem_t) -> int_t {
+    extern { fn sem_destroy(sem: *mut sem_t) -> int_t; }
     unsafe { sem_destroy(sem) }
 }
 
-pub fn sem_getvalue(sem: *mut sem_t, sval: &mut ::int_t) -> ::int_t {
-    extern { fn sem_getvalue(sem: *mut sem_t, sval: *mut ::int_t) -> ::int_t; }
+pub fn sem_getvalue(sem: *mut sem_t, sval: &mut int_t) -> int_t {
+    extern { fn sem_getvalue(sem: *mut sem_t, sval: *mut int_t) -> int_t; }
     unsafe { sem_getvalue(sem, sval as *mut _) }
 }
 
-pub fn sem_init(sem: *mut sem_t, pshared: ::int_t, value: ::uint_t) -> ::int_t {
-    extern { fn sem_init(sem: *mut sem_t, pshared: ::int_t, value: ::uint_t) -> ::int_t; }
+pub fn sem_init(sem: *mut sem_t, pshared: int_t, value: uint_t) -> int_t {
+    extern { fn sem_init(sem: *mut sem_t, pshared: int_t, value: uint_t) -> int_t; }
     unsafe { sem_init(sem, pshared, value) }
 }
 
-pub fn sem_open<T: ::NTStr>(name: &T, oflag: ::int_t, mode: ::sys::types::mode_t,
-                            value: ::uint_t) -> *mut sem_t {
-    extern { fn sem_open(name: *const ::char_t, oflag: ::int_t, ...) -> *mut sem_t; }
+pub fn sem_open<T: NTStr>(name: &T, oflag: int_t, mode: mode_t,
+                            value: uint_t) -> *mut sem_t {
+    extern { fn sem_open(name: *const char_t, oflag: int_t, ...) -> *mut sem_t; }
     unsafe { sem_open(name.as_ptr(), oflag, mode, value) }
 }
 
-pub fn sem_post(sem: *mut sem_t) -> ::int_t {
-    extern { fn sem_post(sem: *mut sem_t) -> ::int_t; }
+pub fn sem_post(sem: *mut sem_t) -> int_t {
+    extern { fn sem_post(sem: *mut sem_t) -> int_t; }
     unsafe { sem_post(sem) }
 }
 
-pub fn sem_timedwait(sem: *mut sem_t, abstime: &::time::timespec) -> ::int_t {
-    extern { fn sem_timedwait(sem: *mut sem_t,
-                              abstime: *const ::time::timespec) -> ::int_t; }
+pub fn sem_timedwait(sem: *mut sem_t, abstime: &timespec) -> int_t {
+    extern { fn sem_timedwait(sem: *mut sem_t, abstime: *const timespec) -> int_t; }
     unsafe { sem_timedwait(sem, abstime as *const _) }
 }
 
-pub fn sem_trywait(sem: *mut sem_t) -> ::int_t {
-    extern { fn sem_trywait(sem: *mut sem_t) -> ::int_t; }
+pub fn sem_trywait(sem: *mut sem_t) -> int_t {
+    extern { fn sem_trywait(sem: *mut sem_t) -> int_t; }
     unsafe { sem_trywait(sem) }
 }
 
-pub fn sem_unlink<T: ::NTStr>(name: &T) -> ::int_t {
-    extern { fn sem_unlink(name: *const ::char_t) -> ::int_t; }
+pub fn sem_unlink<T: NTStr>(name: &T) -> int_t {
+    extern { fn sem_unlink(name: *const char_t) -> int_t; }
     unsafe { sem_unlink(name.as_ptr()) }
 }
 
-pub fn sem_wait(sem: *mut sem_t) -> ::int_t {
-    extern { fn sem_wait(sem: *mut sem_t) -> ::int_t; }
+pub fn sem_wait(sem: *mut sem_t) -> int_t {
+    extern { fn sem_wait(sem: *mut sem_t) -> int_t; }
     unsafe { sem_wait(sem) }
 }
