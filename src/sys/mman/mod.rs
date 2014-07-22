12b6e057e529a582d5a788b06127cb8f724005af
Julian Orth <ju.orth@gmail.com>
2014-07-22 18:17:32 +0200

    sys/mman.h

--- src/sys/mman/mod.rs
+++ src/sys/mman/mod.rs
@@ -17,66 +17,69 @@ pub use self::os::{POSIX_MADV_RANDOM};
 pub use self::os::{POSIX_MADV_SEQUENTIAL};
 pub use self::os::{POSIX_MADV_WILLNEED};
 
+use {void_t, size_t, int_t, NTStr, char_t};
+use sys::types::{off_t, mode_t};
+
 #[cfg(target_os = "linux")]
 #[path = "linux/mod.rs"]
 mod os;
 
-pub fn mlock(addr: *const ::void_t, len: ::size_t) -> ::int_t {
-    extern { fn mlock(addr: *const ::void_t, len: ::size_t) -> ::int_t; }
+pub fn mlock(addr: *const void_t, len: size_t) -> int_t {
+    extern { fn mlock(addr: *const void_t, len: size_t) -> int_t; }
     unsafe { mlock(addr, len) }
 }
 
-pub fn mlockall(flags: ::int_t) -> ::int_t {
-    extern { fn mlockall(flags: ::int_t) -> ::int_t; }
+pub fn mlockall(flags: int_t) -> int_t {
+    extern { fn mlockall(flags: int_t) -> int_t; }
     unsafe { mlockall(flags) }
 }
 
-pub fn mmap(addr: *mut ::void_t, len: ::size_t, prot: ::int_t,
-            flags: ::int_t, fd: ::int_t, offset: ::sys::types::off_t) -> *mut ::void_t {
-    extern { fn mmap(addr: *mut ::void_t, len: ::size_t, prot: ::int_t, flags: ::int_t,
-                     fd: ::int_t, offset: ::sys::types::off_t) -> *mut ::void_t; }
+pub fn mmap(addr: *mut void_t, len: size_t, prot: int_t,
+            flags: int_t, fd: int_t, offset: off_t) -> *mut void_t {
+    extern { fn mmap(addr: *mut void_t, len: size_t, prot: int_t, flags: int_t,
+                     fd: int_t, offset: off_t) -> *mut void_t; }
     unsafe { mmap(addr, len, prot, flags, fd, offset) }
 }
 
-pub fn mprotect(addr: *mut ::void_t, len: ::size_t, prot: ::int_t) -> ::int_t {
-    extern { fn mprotect(addr: *mut ::void_t, len: ::size_t, prot: ::int_t) -> ::int_t; }
+pub fn mprotect(addr: *mut void_t, len: size_t, prot: int_t) -> int_t {
+    extern { fn mprotect(addr: *mut void_t, len: size_t, prot: int_t) -> int_t; }
     unsafe { mprotect(addr, len, prot) }
 }
 
-pub fn msync(addr: *mut ::void_t, len: ::size_t, flags: ::int_t) -> ::int_t {
-    extern { fn msync(addr: *mut ::void_t, len: ::size_t, flags: ::int_t) -> ::int_t; }
+pub fn msync(addr: *mut void_t, len: size_t, flags: int_t) -> int_t {
+    extern { fn msync(addr: *mut void_t, len: size_t, flags: int_t) -> int_t; }
     unsafe { msync(addr, len, flags) }
 }
 
-pub fn munlock(addr: *const ::void_t, len: ::size_t) -> ::int_t {
-    extern { fn munlock(addr: *const ::void_t, len: ::size_t) -> ::int_t; }
+pub fn munlock(addr: *const void_t, len: size_t) -> int_t {
+    extern { fn munlock(addr: *const void_t, len: size_t) -> int_t; }
     unsafe { munlock(addr, len) }
 }
 
-pub fn munlockall() -> ::int_t {
-    extern { fn munlockall() -> ::int_t; }
+pub fn munlockall() -> int_t {
+    extern { fn munlockall() -> int_t; }
     unsafe { munlockall() }
 }
 
-pub fn munmap(addr: *mut ::void_t, len: ::size_t) -> ::int_t {
-    extern { fn munmap(addr: *mut ::void_t, len: ::size_t) -> ::int_t; }
+pub fn munmap(addr: *mut void_t, len: size_t) -> int_t {
+    extern { fn munmap(addr: *mut void_t, len: size_t) -> int_t; }
     unsafe { munmap(addr, len) }
 }
 
-pub fn posix_madvise(addr: *mut ::void_t, len: ::size_t, advice: ::int_t) -> ::int_t {
-    extern { fn posix_madvise(addr: *mut ::void_t, len: ::size_t,
-                              advice: ::int_t) -> ::int_t; }
+pub fn posix_madvise(addr: *mut void_t, len: size_t, advice: int_t) -> int_t {
+    extern { fn posix_madvise(addr: *mut void_t, len: size_t,
+                              advice: int_t) -> int_t; }
     unsafe { posix_madvise(addr, len, advice) }
 }
 
-pub fn shm_open<T: ::NTStr>(name: &T, oflag: ::int_t,
-                            mode: ::sys::types::mode_t) -> ::int_t {
-    extern { fn shm_open(name: *const ::char_t, oflag: ::int_t,
-                         mode: ::sys::types::mode_t) -> ::int_t; }
+pub fn shm_open<T: NTStr>(name: &T, oflag: int_t,
+                            mode: mode_t) -> int_t {
+    extern { fn shm_open(name: *const char_t, oflag: int_t,
+                         mode: mode_t) -> int_t; }
     unsafe { shm_open(name.as_ptr(), oflag, mode) }
 }
 
-pub fn shm_unlink<T: ::NTStr>(name: &T) -> ::int_t {
-    extern { fn shm_unlink(name: *const ::char_t) -> ::int_t; }
+pub fn shm_unlink<T: NTStr>(name: &T) -> int_t {
+    extern { fn shm_unlink(name: *const char_t) -> int_t; }
     unsafe { shm_unlink(name.as_ptr()) }
 }
