f8bb14a1439e1c7920d2f85f505fc0fa48c1f074
Julian Orth <ju.orth@gmail.com>
2014-07-22 15:20:24 +0200

    glob.h

--- src/glob/mod.rs
+++ src/glob/mod.rs
@@ -10,18 +10,19 @@ pub use self::os::{GLOB_ABORTED};
 pub use self::os::{GLOB_NOMATCH};
 pub use self::os::{GLOB_NOSPACE};
 
+use {NTStr, int_t, char_t};
+
 #[cfg(target_os = "linux")]
 #[path = "linux/mod.rs"]
 mod os;
 
-pub fn glob<T: ::NTStr>(pattern: &T, flags: ::int_t,
-                        errfunc: Option<extern fn(arg1: *const ::char_t, arg2: ::int_t) -> ::int_t>,
-                        pglob: &mut glob_t) -> ::int_t {
+pub fn glob<T: NTStr>(pattern: &T, flags: int_t,
+                      errfunc: Option<extern fn(arg1: *const char_t, arg2: int_t) -> int_t>,
+                      pglob: &mut glob_t) -> int_t {
     extern {
-        fn glob(pattern: *const ::char_t, flags: ::int_t,
-                errfunc:
-                    Option<extern fn(arg1: *const ::char_t, arg2: ::int_t) -> ::int_t>,
-                pglob: *mut glob_t) -> ::int_t;
+        fn glob(pattern: *const char_t, flags: int_t,
+                errfunc: Option<extern fn(arg1: *const char_t, arg2: int_t) -> int_t>,
+                pglob: *mut glob_t) -> int_t;
     }
     unsafe { glob(pattern.as_ptr(), flags, errfunc, pglob as *mut _) }
 }
