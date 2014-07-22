a5c31e1d1742884553158fe549dda71a6bb616c9
Julian Orth <ju.orth@gmail.com>
2014-07-22 16:22:21 +0200

    net/if.h

--- src/net/_if/mod.rs
+++ src/net/_if/mod.rs
@@ -1,17 +1,19 @@
 pub use self::os::{if_nameindex};
 pub use self::os::{IF_NAMESIZE};
 
+use {NTStr, uint_t, char_t};
+
 #[cfg(target_os = "linux")]
 #[path = "linux/mod.rs"]
 mod os;
 
-pub fn if_nametoindex<T: ::NTStr>(ifname: &T) -> ::uint_t {
-    extern { fn if_nametoindex(ifname: *const ::char_t) -> ::uint_t; }
+pub fn if_nametoindex<T: NTStr>(ifname: &T) -> uint_t {
+    extern { fn if_nametoindex(ifname: *const char_t) -> uint_t; }
     unsafe { if_nametoindex(ifname.as_ptr()) }
 }
 
-pub fn if_indextoname(ifindex: ::uint_t, dst: &mut [u8]) -> *mut ::char_t {
-    extern { fn if_indextoname(ifindex: ::uint_t, ifname: *mut ::char_t) -> *mut ::char_t; }
+pub fn if_indextoname(ifindex: uint_t, dst: &mut [u8]) -> *mut char_t {
+    extern { fn if_indextoname(ifindex: uint_t, ifname: *mut char_t) -> *mut char_t; }
     if dst.len() < IF_NAMESIZE as uint {
         0 as *mut _
     } else {
