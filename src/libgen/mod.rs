794e42061e6e73b343c2cdeb53ed12749002881b
Julian Orth <ju.orth@gmail.com>
2014-07-22 16:07:34 +0200

    libgen.h

--- src/libgen/mod.rs
+++ src/libgen/mod.rs
@@ -1,9 +1,11 @@
 pub use self::os::{basename};
 
+use {char_t};
+
 #[cfg(target_os = "linux")]
 #[path = "linux/mod.rs"]
 mod os;
 
 extern {
-    pub fn dirname(path: *mut ::char_t) -> *mut ::char_t;
+    pub fn dirname(path: *mut char_t) -> *mut char_t;
 }
