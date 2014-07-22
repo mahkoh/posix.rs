5bf5f1e11164174fb0809ac216b4ff7328b70e66
Julian Orth <ju.orth@gmail.com>
2014-07-22 16:10:45 +0200

    monetary.h

--- src/monetary/mod.rs
+++ src/monetary/mod.rs
@@ -1,6 +1,8 @@
-extern "C" {
-    pub fn strfmon(s: *mut ::char_t, max: ::size_t, format: *const ::char_t,
-                   ...) -> ::ssize_t;
-    pub fn strfmon_l(s: *mut ::char_t, max: ::size_t, loc: ::locale::locale_t,
-                     format: *const ::char_t, ...) -> ::ssize_t;
+use {size_t, char_t, ssize_t};
+use locale::{locale_t};
+
+extern {
+    pub fn strfmon(s: *mut char_t, max: size_t, format: *const char_t, ...) -> ssize_t;
+    pub fn strfmon_l(s: *mut char_t, max: size_t, loc: locale_t, format: *const char_t,
+                     ...) -> ssize_t;
 }
