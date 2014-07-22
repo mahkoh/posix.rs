16afcd4080a71b947997c2216bd83463c41caf14
Julian Orth <ju.orth@gmail.com>
2014-07-22 19:46:35 +0200

    sys/un.h

--- src/sys/un/linux/x86_64.rs
+++ src/sys/un/linux/x86_64.rs
@@ -3,3 +3,8 @@ pub struct sockaddr_un {
     pub sun_family: ::sys::socket::sa_family_t,
     pub sun_path: [::char_t, ..108u],
 }
+
+impl ::AsSlice for sockaddr_un { }
+impl ::AsMutSlice for sockaddr_un { }
+
+new!(sockaddr_un)
