c8c1d6aae576d265b17e4145b24b231e705088ba
Julian Orth <ju.orth@gmail.com>
2014-07-22 19:45:33 +0200

    sys/uio.h

--- src/sys/uio/linux/x86_64.rs
+++ src/sys/uio/linux/x86_64.rs
@@ -3,3 +3,5 @@ pub struct iovec {
     pub iov_base: *mut ::void_t,
     pub iov_len: ::size_t,
 }
+
+new!(iovec)
--- src/sys/uio/mod.rs
+++ src/sys/uio/mod.rs
@@ -1,15 +1,17 @@
 pub use self::os::{iovec};
 
+use {int_t, ssize_t};
+
 #[cfg(target_os = "linux")]
 #[path = "linux/mod.rs"]
 mod os;
 
-pub fn readv(fd: ::int_t, iovec: &[iovec]) -> ::ssize_t {
-    extern { fn readv(fd: ::int_t, iovec: *const iovec, count: ::int_t) -> ::ssize_t; }
-    unsafe { readv(fd, iovec.as_ptr(), iovec.len() as ::int_t) }
+pub fn readv(fd: int_t, iovec: &[iovec]) -> ssize_t {
+    extern { fn readv(fd: int_t, iovec: *const iovec, count: int_t) -> ssize_t; }
+    unsafe { readv(fd, iovec.as_ptr(), iovec.len() as int_t) }
 }
 
-pub fn writev(fd: ::int_t, iovec: &[iovec]) -> ::ssize_t {
-    extern { fn writev(fd: ::int_t, iovec: *const iovec, count: ::int_t) -> ::ssize_t; }
-    unsafe { writev(fd, iovec.as_ptr(), iovec.len() as ::int_t) }
+pub fn writev(fd: int_t, iovec: &[iovec]) -> ssize_t {
+    extern { fn writev(fd: int_t, iovec: *const iovec, count: int_t) -> ssize_t; }
+    unsafe { writev(fd, iovec.as_ptr(), iovec.len() as int_t) }
 }
