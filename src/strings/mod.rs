8291e979d814dca8fbf6366c1abfbb827ccb5604
Julian Orth <ju.orth@gmail.com>
2014-07-22 18:13:07 +0200

    strings.h

--- src/strings/mod.rs
+++ src/strings/mod.rs
@@ -1,13 +1,16 @@
-pub fn ffs(i: ::int_t) -> ::int_t {
-    extern { fn ffs(i: ::int_t) -> ::int_t; }
+use {int_t, size_t, char_t};
+use locale::{locale_t};
+
+pub fn ffs(i: int_t) -> int_t {
+    extern { fn ffs(i: int_t) -> int_t; }
     unsafe { ffs(i) }
 }
 
 extern {
-    pub fn strcasecmp(s1: *const ::char_t, s2: *const ::char_t) -> ::int_t;
-    pub fn strcasecmp_l(s1: *const ::char_t, s2: *const ::char_t,
-                        loc: ::locale::locale_t) -> ::int_t;
-    pub fn strncasecmp(s1: *const ::char_t, s2: *const ::char_t, n: ::size_t) -> ::int_t;
-    pub fn strncasecmp_l(s1: *const ::char_t, s2: *const ::char_t, n: ::size_t,
-                         loc: ::locale::locale_t) -> ::int_t;
+    pub fn strcasecmp(s1: *const char_t, s2: *const char_t) -> int_t;
+    pub fn strcasecmp_l(s1: *const char_t, s2: *const char_t,
+                        loc: locale_t) -> int_t;
+    pub fn strncasecmp(s1: *const char_t, s2: *const char_t, n: size_t) -> int_t;
+    pub fn strncasecmp_l(s1: *const char_t, s2: *const char_t, n: size_t,
+                         loc: locale_t) -> int_t;
 }
