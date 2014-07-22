ae2cb1cb6ea3946182c36254ce44a0431a3d966e
Julian Orth <ju.orth@gmail.com>
2014-07-22 16:49:29 +0200

    nl_types.h

--- src/nl_types/mod.rs
+++ src/nl_types/mod.rs
@@ -3,21 +3,23 @@ pub use self::os::{nl_item};
 pub use self::os::{NL_SETD};
 pub use self::os::{NL_CAT_LOCALE};
 
+use {NTStr, int_t, char_t};
+
 #[cfg(target_os = "linux")]
 #[path = "linux/mod.rs"]
 mod os;
 
-pub fn catopen<T: ::NTStr>(name: &T, flag: ::int_t) -> nl_catd {
-    extern { fn catopen(name: *const ::char_t, flag: ::int_t) -> nl_catd; }
+pub fn catopen<T: NTStr>(name: &T, flag: int_t) -> nl_catd {
+    extern { fn catopen(name: *const char_t, flag: int_t) -> nl_catd; }
     unsafe { catopen(name.as_ptr(), flag) }
 }
 
-pub fn catclose(catalog: nl_catd) -> ::int_t {
-    extern { fn catclose(catalog: nl_catd) -> ::int_t; }
+pub fn catclose(catalog: nl_catd) -> int_t {
+    extern { fn catclose(catalog: nl_catd) -> int_t; }
     unsafe { catclose(catalog) }
 }
 
-extern "C" {
-    pub fn catgets(catalog: nl_catd, set: ::int_t, number: ::int_t,
-                   string: *const ::char_t) -> *mut ::char_t;
+extern {
+    pub fn catgets(catalog: nl_catd, set: int_t, number: int_t,
+                   string: *const char_t) -> *mut char_t;
 }
