a60d32616034da919ed17d8149a563954e9c2df8
Julian Orth <ju.orth@gmail.com>
2014-07-22 16:25:21 +0200

    netdb.h

--- src/netdb/mod.rs
+++ src/netdb/mod.rs
@@ -27,6 +27,10 @@ pub use self::os::{EAI_SOCKTYPE};
 pub use self::os::{EAI_SYSTEM};
 pub use self::os::{EAI_OVERFLOW};
 
+use std::mem::{transmute};
+use {NTStr, int_t, char_t};
+use sys::socket::{sockaddr, socklen_t};
+
 #[cfg(target_os = "linux")]
 #[path = "linux/mod.rs"]
 mod os;
@@ -56,61 +60,60 @@ pub fn freeaddrinfo(ai: *mut addrinfo) {
     unsafe { freeaddrinfo(ai) }
 }
 
-pub fn gai_strerror(ecode: ::int_t) -> *const ::char_t {
-    extern { fn gai_strerror(ecode: ::int_t) -> *const ::char_t; }
+pub fn gai_strerror(ecode: int_t) -> *const char_t {
+    extern { fn gai_strerror(ecode: int_t) -> *const char_t; }
     unsafe { gai_strerror(ecode) }
 }
 
-pub fn getaddrinfo<T: ::NTStr, U: ::NTStr>
-                   (name: &T, service: &U, req: &addrinfo, pai: &mut *mut addrinfo) -> ::int_t {
-    extern { fn getaddrinfo(name: *const ::char_t, service: *const ::char_t,
-                            req: *const addrinfo, pai: *mut *mut addrinfo) -> ::int_t; }
+pub fn getaddrinfo<T: NTStr, U: NTStr>
+                   (name: &T, service: &U, req: &addrinfo, pai: &mut *mut addrinfo) -> int_t {
+    extern { fn getaddrinfo(name: *const char_t, service: *const char_t,
+                            req: *const addrinfo, pai: *mut *mut addrinfo) -> int_t; }
     unsafe { getaddrinfo(name.as_ptr(), service.as_ptr(), req as *const _,
                          pai as *mut _) }
 }
 
 pub fn getnameinfo(sa: &[u8], host: Option<&mut [u8]>, serv: Option<&mut [u8]>,
-                   flags: ::int_t) -> ::int_t {
-    extern { fn getnameinfo(sa: *const ::sys::socket::sockaddr, salen: ::sys::socket::socklen_t,
-                            host: *mut ::char_t, hostlen: ::sys::socket::socklen_t,
-                            serv: *mut ::char_t, servlen: ::sys::socket::socklen_t,
-                            flags: ::int_t) -> ::int_t; }
-    let host_len = host.as_ref().map(|v| v.len() as ::sys::socket::socklen_t).unwrap_or(0);
-    let serv_len = serv.as_ref().map(|v| v.len() as ::sys::socket::socklen_t).unwrap_or(0);
+                   flags: int_t) -> int_t {
+    extern { fn getnameinfo(sa: *const sockaddr, salen: socklen_t,
+                            host: *mut char_t, hostlen: socklen_t,
+                            serv: *mut char_t, servlen: socklen_t,
+                            flags: int_t) -> int_t; }
+    let host_len = host.as_ref().map(|v| v.len() as socklen_t).unwrap_or(0);
+    let serv_len = serv.as_ref().map(|v| v.len() as socklen_t).unwrap_or(0);
     let host_ptr = host.map(|v| v.as_mut_ptr() as *mut _).unwrap_or(0 as *mut _);
     let serv_ptr = serv.map(|v| v.as_mut_ptr() as *mut _).unwrap_or(0 as *mut _);
     if host_len + serv_len == 0 {
         return EAI_OVERFLOW;
     }
-    unsafe { getnameinfo(sa.as_ptr() as *const _, sa.len() as ::sys::socket::socklen_t,
+    unsafe { getnameinfo(sa.as_ptr() as *const _, sa.len() as socklen_t,
                          host_ptr, host_len, serv_ptr, serv_len, flags) }
 }
 
-pub fn getnetbyaddr(net: u32, ty: ::int_t) -> &'static mut netent {
-    extern { fn getnetbyaddr(net: u32, ty: ::int_t) -> *mut netent; }
-    unsafe { ::std::mem::transmute(getnetbyaddr(net, ty)) }
+pub fn getnetbyaddr(net: u32, ty: int_t) -> &'static mut netent {
+    extern { fn getnetbyaddr(net: u32, ty: int_t) -> *mut netent; }
+    unsafe { transmute(getnetbyaddr(net, ty)) }
 }
 
-pub fn getnetbyname<T: ::NTStr>(name: &T) -> &'static mut netent {
-    extern { fn getnetbyname(name: *const ::char_t) -> *mut netent; }
-    unsafe { ::std::mem::transmute(getnetbyname(name.as_ptr())) }
+pub fn getnetbyname<T: NTStr>(name: &T) -> &'static mut netent {
+    extern { fn getnetbyname(name: *const char_t) -> *mut netent; }
+    unsafe { transmute(getnetbyname(name.as_ptr())) }
 }
 
-pub fn getprotobyname<T: ::NTStr>(name: &T) -> &'static mut protoent {
-    extern { fn getprotobyname(name: *const ::char_t) -> *mut protoent; }
-    unsafe { ::std::mem::transmute(getprotobyname(name.as_ptr())) }
+pub fn getprotobyname<T: NTStr>(name: &T) -> &'static mut protoent {
+    extern { fn getprotobyname(name: *const char_t) -> *mut protoent; }
+    unsafe { transmute(getprotobyname(name.as_ptr())) }
 }
 
-pub fn getprotobynumber(proto: ::int_t) -> &'static mut protoent {
-    extern { fn getprotobynumber(proto: ::int_t) -> *mut protoent; }
-    unsafe { ::std::mem::transmute(getprotobynumber(proto)) }
+pub fn getprotobynumber(proto: int_t) -> &'static mut protoent {
+    extern { fn getprotobynumber(proto: int_t) -> *mut protoent; }
+    unsafe { transmute(getprotobynumber(proto)) }
 }
 
-pub fn getservbyname<T: ::NTStr, U: ::NTStr>(name: &T,
-                                             proto: &U) -> &'static mut servent {
-    extern { fn getservbyname(name: *const ::char_t,
-                              proto: *const ::char_t) -> *mut servent; }
-    unsafe { ::std::mem::transmute(getservbyname(name.as_ptr(), proto.as_ptr())) }
+pub fn getservbyname<T: NTStr, U: NTStr>(name: &T, proto: &U) -> &'static mut servent {
+    extern { fn getservbyname(name: *const char_t,
+                              proto: *const char_t) -> *mut servent; }
+    unsafe { transmute(getservbyname(name.as_ptr(), proto.as_ptr())) }
 }
 
 extern "C" {
@@ -118,8 +121,8 @@ extern "C" {
     pub fn getnetent() -> *mut netent;
     pub fn getprotoent() -> *mut protoent;
     pub fn getservent() -> *mut servent;
-    pub fn sethostent(stay_open: ::int_t);
-    pub fn setnetent(stay_open: ::int_t);
-    pub fn setprotoent(stay_open: ::int_t);
-    pub fn setservent(stay_open: ::int_t);
+    pub fn sethostent(stay_open: int_t);
+    pub fn setnetent(stay_open: int_t);
+    pub fn setprotoent(stay_open: int_t);
+    pub fn setservent(stay_open: int_t);
 }
