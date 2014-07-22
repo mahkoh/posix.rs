ed4fa46d1403e4d0559ce26c66e4737f0b96bfca
Julian Orth <ju.orth@gmail.com>
2014-07-22 16:21:00 +0200

    ndbm.h

--- src/ndbm/mod.rs
+++ src/ndbm/mod.rs
@@ -3,12 +3,14 @@ pub use self::os::DBM;
 pub use self::os::{DBM_INSERT};
 pub use self::os::{DBM_REPLACE};
 
+use {NTStr, int_t, char_t};
+
 #[cfg(target_os = "linux")]
 #[path = "linux/mod.rs"]
 mod os;
 
-pub fn dbm_open<T: ::NTStr>(file: &T, flags: ::int_t, mode: ::int_t) -> *mut DBM {
-    extern { fn dbm_open(file: *const ::char_t, flags: ::int_t, mode: ::int_t) -> *mut DBM; }
+pub fn dbm_open<T: NTStr>(file: &T, flags: int_t, mode: int_t) -> *mut DBM {
+    extern { fn dbm_open(file: *const char_t, flags: int_t, mode: int_t) -> *mut DBM; }
     unsafe { dbm_open(file.as_ptr(), flags, mode) }
 }
 
@@ -22,14 +24,14 @@ pub fn dbm_fetch(dbf: *mut DBM, key: datum) -> datum {
     unsafe { dbm_fetch(dbf, key) }
 }
 
-pub fn dbm_store(dbf: *mut DBM, key: datum, content: datum, flags: ::int_t) -> ::int_t {
+pub fn dbm_store(dbf: *mut DBM, key: datum, content: datum, flags: int_t) -> int_t {
     extern { fn dbm_store(dbf: *mut DBM, key: datum, content: datum,
-                          flags: ::int_t) -> ::int_t; }
+                          flags: int_t) -> int_t; }
     unsafe { dbm_store(dbf, key, content, flags) }
 }
 
-pub fn dbm_delete(dbf: *mut DBM, key: datum) -> ::int_t {
-    extern { fn dbm_delete(dbf: *mut DBM, key: datum) -> ::int_t; }
+pub fn dbm_delete(dbf: *mut DBM, key: datum) -> int_t {
+    extern { fn dbm_delete(dbf: *mut DBM, key: datum) -> int_t; }
     unsafe { dbm_delete(dbf, key) }
 }
 
@@ -43,8 +45,8 @@ pub fn dbm_nextkey(dbf: *mut DBM) -> datum {
     unsafe { dbm_nextkey(dbf) }
 }
 
-pub fn dbm_error(dbf: *mut DBM) -> ::int_t {
-    extern { fn dbm_error(dbf: *mut DBM) -> ::int_t; }
+pub fn dbm_error(dbf: *mut DBM) -> int_t {
+    extern { fn dbm_error(dbf: *mut DBM) -> int_t; }
     unsafe { dbm_error(dbf) }
 }
 
