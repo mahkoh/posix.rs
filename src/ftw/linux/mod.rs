9ee3de1434f3147ba7332ceb3656a6d9b52f9f09
Julian Orth <ju.orth@gmail.com>
2014-07-23 15:32:41 +0200

    def.toml

---
+++ src/ftw/linux/mod.rs
@@ -0,0 +1,16 @@
+pub use self::arch::{FTW};
+pub use self::arch::{FTW_F};
+pub use self::arch::{FTW_D};
+pub use self::arch::{FTW_DNR};
+pub use self::arch::{FTW_DP};
+pub use self::arch::{FTW_NS};
+pub use self::arch::{FTW_SL};
+pub use self::arch::{FTW_SLN};
+pub use self::arch::{FTW_PHYS};
+pub use self::arch::{FTW_MOUNT};
+pub use self::arch::{FTW_DEPTH};
+pub use self::arch::{FTW_CHDIR};
+
+#[cfg(target_arch = "x86_64")]
+#[path = "x86_64.rs"]
+mod arch;
